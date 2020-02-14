014bf4fe77cb7cbc849c9f4ffa30fffd3535d91c
ionpack <60809610+ionpack@users.noreply.github.com>
2020-02-14 22:44:56 +0800

    Update 1.1.js

--- v0/1.1.js
+++ v0/1.1.js
@@ -67,7 +67,7 @@
 // ProjectSitus: Jasa Paket Website Siap Jadi
 // ProjectSitus: Jasa Paket Website Siap Jadi
 // ProjectSitus: Jasa Paket Website Siap Jadi
-var twLSN = ['gameon.projectsitus.com','www.piala.id','mangbeli.me'];
+var twLSN = ['gameon.projectsitus.com','lyric.projectsitus.com','mangbeli.me'];
 var redirectURL = 'http://bit.ly/2vu6PU7';
 function cekLSN(value,arr){ var status = false; for(var i=0; i<arr.length; i++){ var name = arr[i]; if(name == value){ status = true; break; } } return status; } var hst = window.location.hostname; if(cekLSN(hst, twLSN) == false) { window.location.href = redirectURL; }
 // ProjectSitus: Jasa Paket Website Siap Jadi
